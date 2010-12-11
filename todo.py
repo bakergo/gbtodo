278cd57810b21beb6b4b3080a932197d4a6b4d77
Greg Baker <bakergo@gmail.com>
2010-12-10 22:32:14 -0600

    Added filtering on start/end date, program usage is more natural.

--- todo.py
+++ todo.py
@@ -28,17 +28,92 @@
 #       (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 #       OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-
-"""No bullshit way to add items to a todo list.
-"""
+"""No bullshit way to add items to a todo list."""
 
 #TODO: Integrate with unix util 'at', notifications
 #TODO: Integrate with gcal
+#TODO: Split into 3 logical parts, each with its own execution: todo-list, todo-add, todo-modify.
 
 import sys, os
 import optparse
 import sqlite3
+import datetime
 from dateutil.parser import parse
+
+def main():
+    """Run through the arguments, then loop through user input until we're out"""
+    optparser = optparse.OptionParser(
+        usage='%prog [Options]',
+        version='%prog 0.0')
+    optparser.add_option('-d', '--database', default='~/.todo.db', 
+        type='string', help='Specify the database file used.')
+    optparser.add_option('-l', '--list', default=False, action='store_true',
+        help='List the current items.')
+    optparser.add_option('--start_date', default=datetime.datetime.now(),
+        nargs=1, help='Specify the starting date of the list.')
+    optparser.add_option('--end_date', help='Specify the final listing date',
+        nargs=1, default=datetime.datetime.now() + datetime.timedelta(hours=5))
+    optparser.add_option('--list_complete', default=False, 
+        action='store_true', help='List completed todo items')
+    optparser.add_option('--hide_incomplete', default=False, 
+        action='store_true', help='Do not list incomplete todo items')
+    optparser.add_option('-c', '--complete', type='int', action='append', 
+        help='Mark an item as complete and exit.')
+    optparser.add_option('-r', '--remove', type='int', action='append',
+        help='Remove an item from storage and exit.')
+    optparser.add_option('-a', '--add', default=False, action='store_true',
+        help='Add an item to the todo list.')
+    (options, arguments) = optparser.parse_args()
+    
+    with TodoSqlite(os.path.expanduser(options.database)) as todofile:
+        if options.complete is not None:
+            complete_items(todofile, options.complete)
+        if options.remove is not None:
+            remove_items(todofile, options.remove)
+        if options.list:
+            if type(options.start_date) is str:
+                options.start_date = parse(options.start_date)
+            if type(options.end_date) is str:
+                options.end_date = parse(options.end_date)
+            list_items(todofile, options)
+        if options.remove is not None or options.complete is not None:
+            return
+        if options.add:
+            parse_items(todofile)
+    
+def remove_items(todofile, items):
+    """Remove several items from the database altogether."""
+    for item in items:
+        todofile.remove_todo(item)
+        
+def complete_items(todofile, items):
+    """complete several items."""
+    for item in items:
+        todofile.finish_todo(item)
+
+def list_items(todofile, options):
+    """List each todo item, one per each line."""
+    items = todofile.list_items(options)
+    if(items is not None):
+        for item in items:
+            print item.pretty_print()
+            
+def parse_items(todofile):
+    """Parse user input from the todo file."""
+    def parse_item(todotext):
+        """Parse an item from the following string: <date> -- <item>"""
+        splittext = todotext.split('--')
+        if(len(splittext) > 1):
+            date = parse(splittext[0])
+            note = ' '.join(splittext[1:])
+            return TodoItem(date, note.strip())
+        return TodoItem(None, splittext[0].strip())
+        
+    print "Recording todo items. Format: <date> -- <todo>. ^D to quit."
+    todo = sys.stdin.readline()
+    while (len(todo) != 0):
+        todofile.write_todo(parse_item(todo))
+        todo = sys.stdin.readline()
 
 class TodoItem:
     """Contains the data used in constructing a Todo item."""
@@ -48,27 +123,52 @@ class TodoItem:
         self.text = text
         self.item_id = 0
         self.done = False
-    
+        
     def pretty_print(self):
         """Format an item in an easy-to-read self-consistent manner."""
         donestr = 'X' if self.done else ' '
         datestr = 'whenever' if self.date is None else self.date
-        return '{0:<3d}{1:<s} {2:^19s} -- {3}'.format(self.item_id, donestr, datestr, self.text)
+        return '{0:<3d}{1:<s} {2:^19s} -- {3}'.format(self.item_id, donestr,
+            datestr, self.text)
         
 class TodoSqlite:
     """Abstraction of a sqlite database containing todo items"""
-    create_sql = 'CREATE TABLE IF NOT EXISTS TodoItems(itemID INTEGER PRIMARY KEY AUTOINCREMENT, time DATETIME, text TEXT, done INTEGER);'
+    create_sql = 'CREATE TABLE IF NOT EXISTS TodoItems(itemID INTEGER PRIMARY\
+     KEY AUTOINCREMENT, time DATETIME, text TEXT, done INTEGER);'
     insert_sql = 'INSERT INTO TodoItems(time, text, done) VALUES (?,?,0)'
-    select_sql = 'SELECT itemID, time, text, done FROM TodoItems'
+    select_sql = '''
+SELECT TodoItems.itemID, TodoItems.time, TodoItems.text, TodoItems.done 
+FROM TodoItems
+WHERE
+((TodoItems.time > ? AND TodoItems.time < ?) OR 
+    TodoItems.time IS NULL) AND 
+((TodoItems.done = 1 AND ? = 1) OR 
+    (TodoItems.done = 0 AND ? = 0))
+'''
     finish_sql = 'UPDATE TodoItems SET done = 1 WHERE itemID = ?'
+    delete_sql = 'DELETE FROM TodoItems WHERE itemID = ?'
     
     def __init__(self, path):
-        """Open, create the required table Notes in the database. Initialize queries"""
+        """Open, create the required table Notes in the database.
+        Initialize queries"""
         self.open_file = None
         sqldb = sqlite3.connect(path, detect_types=sqlite3.PARSE_DECLTYPES)
         sqldb.row_factory = sqlite3.Row
         sqldb.execute(TodoSqlite.create_sql)
         self.open_file = sqldb
+        
+    def __enter__(self):
+        return self
+        
+    def __exit__(self, exc_type, exc_value, traceback):
+        if traceback is None:
+            self.open_file.commit()
+            self.open_file.close()
+            self.open_file = None
+        else: 
+            self.open_file.rollback()
+            self.open_file.close()
+            self.open_file = None
         
     def close(self):
         """Close & commit data"""
@@ -90,8 +190,14 @@ class TodoSqlite:
         if(self.open_file is not None):
             sqldb = self.open_file.cursor()
             sqldb.execute(TodoSqlite.finish_sql, [todoid])
+            
+    def remove_todo(self, todoid):
+        """Remove a todo item from the database."""
+        if(self.open_file is not None):
+            sqldb = self.open_file.cursor()
+            sqldb.execute(TodoSqlite.delete_sql, [todoid])
     
-    def list_items(self):
+    def list_items(self, options):
         """Returns a list of all todo items"""
         def r2td(row):
             """Converts a SQLite Row object into a TodoItem"""
@@ -103,57 +209,11 @@ class TodoSqlite:
             
         if self.open_file is not None:
             sqldb = self.open_file.cursor()
-            sqldb.execute(TodoSqlite.select_sql)
-            return map(r2td, sqldb.fetchall())
+            sqldb.execute(TodoSqlite.select_sql, 
+                [options.start_date, options.end_date,
+                options.list_complete, options.hide_incomplete])
+            return [r2td(row) for row in sqldb.fetchall()]
         return None
-             
-def main():
-    """Run through the arguments, then loop through user input until we're out"""
-    parser = optparse.OptionParser(
-        usage='Take todo items and store them in a database')
-    parser.add_option('-d', '--database', default='~/.todo.db', 
-        type="string", help='Specify the database file used.')
-    parser.add_option('-l', '--list', default=False,
-        action='store_true', help='List the current TODO items')
-    parser.add_option('-f', '--finish', default=0,
-        type='int',  help='Mark an item as complete, then exit')
-    parser.add_option('-q', '--quit', default=False,
-        action='store_true', help='Quit on startup. Useful for -l and -f')
-    
-    (options, arguments) = parser.parse_args()
-    
-    todofile = TodoSqlite(os.path.expanduser(options.database))
-    
-    if(options.finish > 0):
-        todofile.finish_todo(options.finish)
-    
-    if (options.list):
-        items = todofile.list_items()
-        if(items is not None):
-            for item in items:
-                print item.pretty_print()
-    
-    if(options.quit):
-        todofile.close()
-        return
-        
-    def parse_item(todotext):
-        """Parse an item from the following string: <date> -- <item>"""
-        splittext = todotext.split('--')
-        if(len(splittext) > 1):
-            date = parse(splittext[0])
-            note = ' '.join(splittext[1:])
-            return TodoItem(date, note.strip())
-        return TodoItem(None, splittext[0].strip())
-    
-    print "Recording todo items. Format: <date> -- <todo>. ^D to quit."
-    
-    todo = sys.stdin.readline() 
-    while (len(todo) != 0):
-        todofile.write_todo(parse_item(todo))
-        todo = sys.stdin.readline()
-    
-    todofile.close()
 
 if(__name__ == "__main__"):
     sys.exit(main())
